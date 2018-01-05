5de93ac5e64ab68266e87fa5b7b8e36b011b767c
Simon Martin <havvoric@gmail.com>
2018-01-05 11:04:27 +0000

    Dump POST entity.

--- queue/src/queue_main.cpp
+++ queue/src/queue_main.cpp
@@ -24,25 +24,56 @@ using boost::property_tree::read_json;
 using boost::property_tree::write_json;
 
 #define PORT 8888
+struct connection_data {
+  connection_data():is_parsing(false),entity() {}
 
-int print_out_key(void *cls, enum MHD_ValueKind kind, const char *key, const char *value) {
+  bool is_parsing;
+  std::string entity;
+};
+
+int print_out_key(void * /*cls*/, enum MHD_ValueKind /*kind*/, const char *key, const char *value) {
   fprintf(stderr, "%s: %s\n", key, value);
   return MHD_YES;
 }
 
 static int
-answer_to_connection (void *cls, struct MHD_Connection *connection,
+answer_to_connection (void * /*cls*/, struct MHD_Connection *connection,
                       const char *url, const char *method,
                       const char *version, const char *upload_data,
                       size_t *upload_data_size, void **con_cls)
 {
-/*
-  const char *page = "<html><body>Hello, browser!</body></html>";
-*/
-  fprintf(stderr, "%s %s %s\n", method, url, version);
-
-  MHD_get_connection_values(connection, MHD_HEADER_KIND, &print_out_key, NULL);
   static int call_count = 0;
+
+  if(strcmp(method, MHD_HTTP_METHOD_POST) == 0) {
+    connection_data *data = static_cast<connection_data*>(*con_cls);
+    if(nullptr == data) {
+      data = new connection_data();
+      data->is_parsing = false;
+      *con_cls = data;
+    }
+
+    if(!data->is_parsing) {
+      data->is_parsing = true;
+      return MHD_YES;
+    }
+
+    if(*upload_data_size != 0) {
+      data->entity += std::string(upload_data, *upload_data_size);
+      *upload_data_size = 0;
+      return MHD_YES;
+    }
+
+    fprintf(stderr, "%s %s %s\n", method, url, version);
+
+    MHD_get_connection_values(connection, MHD_HEADER_KIND, &print_out_key, NULL);
+    fprintf(stderr, "> %s\n", data->entity.c_str());
+    delete data;
+    *con_cls = nullptr;
+  } else {
+    fprintf(stderr, "%s %s %s\n", method, url, version);
+
+    MHD_get_connection_values(connection, MHD_HEADER_KIND, &print_out_key, NULL);
+  }
   
   ptree pt;
   pt.put ("count", ++ call_count);
@@ -55,6 +86,8 @@ answer_to_connection (void *cls, struct MHD_Connection *connection,
   MHD_add_response_header(response, MHD_HTTP_HEADER_CONTENT_TYPE, "application/json");
   int ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
   MHD_destroy_response (response);
+
+  fprintf(stderr, "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
 
   return ret;
 }
