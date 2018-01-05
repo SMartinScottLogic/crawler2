aa3a0cf50edd5c41aaf77ff24cfe55241d87e549
Simon Martin <havvoric@gmail.com>
2018-01-05 11:54:06 +0000

    Parsing POST entity as JSON.

--- queue/src/queue_main.cpp
+++ queue/src/queue_main.cpp
@@ -28,12 +28,23 @@ struct connection_data {
   connection_data():is_parsing(false),entity() {}
 
   bool is_parsing;
-  std::string entity;
+  std::stringstream entity;
 };
 
 int print_out_key(void * /*cls*/, enum MHD_ValueKind /*kind*/, const char *key, const char *value) {
   fprintf(stderr, "%s: %s\n", key, value);
   return MHD_YES;
+}
+
+void print_tree(const ptree &pt, int level) {
+  if(pt.empty()) {
+    fprintf(stderr, "value(%d): \"%s\"\n", level, pt.data().c_str());
+    return;
+  }
+  for(auto entry : pt) {
+    fprintf(stderr, "key(%d): \"%s\"\n", level, entry.first.c_str());
+    print_tree(entry.second, level+1);
+  }
 }
 
 static int
@@ -58,7 +69,7 @@ answer_to_connection (void * /*cls*/, struct MHD_Connection *connection,
     }
 
     if(*upload_data_size != 0) {
-      data->entity += std::string(upload_data, *upload_data_size);
+      data->entity << std::string(upload_data, *upload_data_size);
       *upload_data_size = 0;
       return MHD_YES;
     }
@@ -66,7 +77,13 @@ answer_to_connection (void * /*cls*/, struct MHD_Connection *connection,
     fprintf(stderr, "%s %s %s\n", method, url, version);
 
     MHD_get_connection_values(connection, MHD_HEADER_KIND, &print_out_key, NULL);
-    fprintf(stderr, "> %s\n", data->entity.c_str());
+    fprintf(stderr, "> %s\n", data->entity.str().c_str());
+    std::stringstream ss;
+
+    ptree pt;
+    read_json(data->entity, pt);
+    print_tree(pt, 1);
+    fprintf(stderr, "queue: %s\n", pt.get<std::string>("queue").c_str());
     delete data;
     *con_cls = nullptr;
   } else {
